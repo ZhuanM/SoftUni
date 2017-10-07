05646efd8c8e5c340efdd031a43fbbae5b777ea4
Zhuan <jowan@abv.bg>
2017-10-07 17:27:35 +0300

    methods lab done

--- Programming Fundamentals/04. Methods. Debugging and Troubleshooting Code/Lab/09. Multiply Evens by Odds/Program.cs
+++ Programming Fundamentals/04. Methods. Debugging and Troubleshooting Code/Lab/09. Multiply Evens by Odds/Program.cs
@@ -8,11 +8,49 @@ namespace _09.Multiply_Evens_by_Odds
 {
     class Program
     {
+        static int GetSumOfEvenDigits(int n)
+        {
+            int sum = 0;
+            int lastDigit = 0;
+            while (n > 0)
+            {
+                lastDigit = n % 10;
+                if (n % 2 == 0)
+                {
+                    sum += lastDigit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
 
+        static int GetSumOfOddDigits(int n)
+        {
+            int sum = 0;
+            int lastDigit = 0;
+            while (n > 0)
+            {
+                lastDigit = n % 10;
+                if (n % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+
+        static int GetMultipleOfEvenAndOdds(int n)
+        {
+            int evenSum = GetSumOfEvenDigits(n);
+            int oddSum = GetSumOfOddDigits(n);
+            return evenSum * oddSum;
+        }
 
         static void Main(string[] args)
         {
-
+            int n = Math.Abs(int.Parse(Console.ReadLine()));
+            Console.WriteLine(GetMultipleOfEvenAndOdds(n));
         }
     }
 }
---
+++ Programming Fundamentals/04. Methods. Debugging and Troubleshooting Code/Lab/11. Price Change Alert/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Price_Change_Alert
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double threshold = double.Parse(Console.ReadLine());
+            double lastPrice = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                double currPrice = double.Parse(Console.ReadLine());
+                double div = Proc(lastPrice, currPrice);
+                bool isSignificantDifference = isThereDiff(div, threshold);
+                string message = Get(currPrice, lastPrice, div, isSignificantDifference);
+
+                Console.WriteLine(message);
+                lastPrice = currPrice;
+            }
+        }
+
+        private static string Get(double currPrice, double lastPrice, double razlika, bool etherTrueOrFalse)
+        {
+            string to = "";
+            if (razlika == 0)
+            {
+                to = string.Format("NO CHANGE: {0}", currPrice);
+            }
+            else if (!etherTrueOrFalse)
+            {
+                to = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)",
+                    lastPrice, currPrice, razlika * 100);
+            }
+            else if (etherTrueOrFalse && (razlika > 0))
+            {
+                to = string.Format("PRICE UP: {0} to {1} ({2:F2}%)",
+                    lastPrice, currPrice, razlika * 100);
+            }
+            else if (etherTrueOrFalse && (razlika < 0))
+                to = string.Format("PRICE DOWN: {0} to {1} ({2:F2}%)",
+                    lastPrice, currPrice, razlika * 100);
+            return to;
+        }
+
+        private static bool isThereDiff(double threshold, double isDiff)
+        {
+            if (Math.Abs(threshold) >= isDiff)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static double Proc(double lastPrice, double currPrice)
+        {
+            double result = (currPrice - lastPrice) / lastPrice;
+            return result;
+        }
+    }
+}
