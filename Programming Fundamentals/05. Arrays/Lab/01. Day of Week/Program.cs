3cff901b4f97c7baee5681031d76a53af9322e35
Zhuan <jowan@abv.bg>
2017-10-09 19:50:54 +0300

    Arrays lab started

---
+++ Programming Fundamentals/05. Arrays/Lab/01. Day of Week/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            string[] days =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if (1 <= day && day <= 7)
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Day!");
+            }
+        }
+    }
+}
