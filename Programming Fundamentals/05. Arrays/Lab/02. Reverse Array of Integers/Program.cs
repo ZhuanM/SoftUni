c99bc6a48fd193b7f1afd67be0b083ca6a9a05d3
Zhuan <jowan@abv.bg>
2017-10-10 19:43:05 +0300

    Arrays lab

--- Programming Fundamentals/05. Arrays/Lab/02. Reverse Array of Integers/Program.cs
+++ Programming Fundamentals/05. Arrays/Lab/02. Reverse Array of Integers/Program.cs
@@ -10,7 +10,17 @@ namespace _02.Reverse_Array_of_Integers
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(arr);
+
+            Console.WriteLine(string.Join(" ", arr));
         }
     }
 }
