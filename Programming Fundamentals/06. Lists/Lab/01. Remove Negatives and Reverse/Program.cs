29d373edb18f51c9ab1c243bb3bd58b27e62df96
Zhuan <jowan@abv.bg>
2017-10-14 22:21:19 +0300

    Lists lab started

---
+++ Programming Fundamentals/06. Lists/Lab/01. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    result.Insert(0, nums[i]);
+                }
+            }
+
+            if (result.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }      
+        }
+    }
+}
---
+++ Programming Fundamentals/06. Lists/Lab/02. Append Lists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Append_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lists = Console.ReadLine().Split('|');
+            List<int> result = new List<int>();
+
+            for (int i = lists.Length - 1; i >= 0; i--)
+            {
+                int[] elements = lists[i]
+                    .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                result.AddRange(elements);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
