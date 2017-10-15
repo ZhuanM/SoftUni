c64e7b25cccadc5267cd3c8ff4335451f8213a40
Zhuan <jowan@abv.bg>
2017-10-15 14:26:35 +0300

    Minor changges

--- Programming Fundamentals/06. Lists/Lab/07. Count Numbers/Program.cs
+++ Programming Fundamentals/06. Lists/Lab/07. Count Numbers/Program.cs
@@ -15,7 +15,27 @@ namespace _07.Count_Numbers
                 .Select(int.Parse)
                 .ToList();
 
+            nums.Sort();
+            List<int> passed = new List<int>();
 
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (!passed.Contains(nums[i]))
+                {
+                    int count = 0;
+
+                    for (int j = i; j < nums.Count; j++)
+                    {
+                        if (nums[j] == nums[i])
+                        {
+                            count++;
+                        }
+                    }
+
+                    Console.WriteLine($"{nums[i]} -> {count}");
+                    passed.Add(nums[i]);
+                }
+            }
         }
     }
 }
