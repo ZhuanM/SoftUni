e0f5b297570dc6c550b1268b6c5111f75facca89
Zhuan <jowan@abv.bg>
2017-10-20 22:04:13 +0300

    Dictionaries and lambda and linq exercises started

---
+++ Programming Fundamentals/07. Dictionaries, Lambda and LINQ/Exercises/01. Phonebook/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Trim().Split(new [] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var phonebook = new Dictionary<string, string>();
+
+            while (command[0] != "END")
+            {
+                if (command[0] == "A")
+                {
+                    phonebook[command[1]] = command[2];
+                }
+                else if (command[0] == "S")
+                {
+                    if (phonebook.ContainsKey(command[1]))
+                    {
+                        Console.WriteLine($"{command[1]} -> {phonebook[command[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {command[1]} does not exist.");
+                    }
+                }
+
+
+                command = Console.ReadLine().Trim().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
