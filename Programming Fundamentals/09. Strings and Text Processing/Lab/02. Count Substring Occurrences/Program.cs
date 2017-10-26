fdbf7b1142c5a43a96b35e057504bfff6255ccee
Zhuan <jowan@abv.bg>
2017-10-26 20:37:49 +0300

    string manipulation lab done

--- Programming Fundamentals/09. Strings and Text Processing/Lab/02. Count Substring Occurrences/Program.cs
+++ Programming Fundamentals/09. Strings and Text Processing/Lab/02. Count Substring Occurrences/Program.cs
@@ -10,7 +10,20 @@ namespace _02.Count_Substring_Occurrences
     {
         static void Main(string[] args)
         {
+            string text = Console.ReadLine().ToLower();
+            string pattern = Console.ReadLine().ToLower();
 
+            int index = text.IndexOf(pattern);
+
+            int count = 0;
+
+            while (index != -1)
+            {
+                count++;
+                index = text.IndexOf(pattern, index + 1);
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
---
+++ Programming Fundamentals/09. Strings and Text Processing/Lab/03. Text Filter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var badWords = Console.ReadLine()
+                .Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var text = Console.ReadLine();
+
+            foreach (var badWord in badWords)
+            {
+                if (text.Contains(badWord))
+                {
+                    text = text.Replace(badWord, new string('*', badWord.Length));
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
