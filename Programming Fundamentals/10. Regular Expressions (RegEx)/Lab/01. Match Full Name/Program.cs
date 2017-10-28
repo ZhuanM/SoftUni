b0f5c1701926eab1a0c6f1dc07cfdbf7ad8ac2e9
Zhuan <jowan@abv.bg>
2017-10-28 22:58:05 +0300

    Regex lab almost done

---
+++ Programming Fundamentals/10. Regular Expressions (RegEx)/Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+\s[A-Z][a-z]+\b";          
+
+            foreach (Match name in Regex.Matches(input, pattern))
+            {
+                Console.Write(name.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Programming Fundamentals/10. Regular Expressions (RegEx)/Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _02.Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359(-|\s)2\1\d{3}\1\d{4}\b";
+            string input = Console.ReadLine();
+
+            MatchCollection phoneMatches = Regex.Matches(input, pattern);
+
+            string[] listOfNums = phoneMatches.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", listOfNums));
+        }
+    }
+}
