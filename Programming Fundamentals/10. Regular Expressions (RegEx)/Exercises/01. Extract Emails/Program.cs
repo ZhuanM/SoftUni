d9cdcfb5611b72e76172d9c6720efe420b5b76b8
Zhuan <jowan@abv.bg>
2017-10-29 20:49:11 +0300

    regex exercises almost done

---
+++ Programming Fundamentals/10. Regular Expressions (RegEx)/Exercises/01. Extract Emails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern =
+                @"(?<=\s|^)([A-Za-z0-9]+[._-]?[A-Za-z0-9]+)+@([A-Za-z]+[._-]?)+\.[A-Za-z]+(?=[.,\s]|\b)";
+            string input = Console.ReadLine();
+
+            MatchCollection emails = Regex.Matches(input, pattern);
+
+            foreach (Match emailMatch in emails)
+            {
+                Console.WriteLine(emailMatch.Value);
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/10. Regular Expressions (RegEx)/Exercises/03. Camera View/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03.Camera_View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int skipElements = firstInput[0];
+            int takeElements = firstInput[1];
+
+            string input = Console.ReadLine();
+
+            Regex cameraGroups = 
+                new Regex(@"\|<(\w{" + skipElements + @"})(\w{0," + takeElements + @"})");
+
+            MatchCollection solution = cameraGroups.Matches(input);
+
+            List<string> picsTaken = new List<string>();
+
+            foreach (Match camera in solution)
+            {
+                picsTaken.Add(camera.Groups[2].Value.ToString());
+            }
+
+            Console.WriteLine(string.Join(", ", picsTaken));
+        }
+    }
+}
