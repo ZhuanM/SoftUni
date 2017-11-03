61c407631303182c3a4c571099b852da5084546f
Zhuan <jowan@abv.bg>
2017-11-03 17:48:46 +0300

    examp prep III finished

--- Programming Fundamentals/Exam Preparation III/03. Rage Quit/Program.cs
+++ Programming Fundamentals/Exam Preparation III/03. Rage Quit/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _03.Rage_Quit
@@ -10,7 +11,28 @@ namespace _03.Rage_Quit
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            string pattern = @"(\D+)(\d+)";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (Match m in matches)
+            {
+                string word = m.Groups[1].Value.ToUpper();
+                int count = int.Parse(m.Groups[2].Value);
+
+                for (int i = 0; i < count; i++)
+                {
+                    result.Append(word);
+                }                
+            }
+
+            int countInResult = result.ToString().Distinct().Count();
+
+            Console.WriteLine($"Unique symbols used: {countInResult}");
+            Console.WriteLine(result);
         }
     }
 }
--- Programming Fundamentals/Exam Preparation III/04. Files/Program.cs
+++ Programming Fundamentals/Exam Preparation III/04. Files/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _04.Files
@@ -10,7 +11,50 @@ namespace _04.Files
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            List<string> allFiles = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                allFiles.Add(Console.ReadLine());
+            }
+
+            string filter = Console.ReadLine();
+            var filterTokens = Regex.Split(filter, " in ");
+            var filterExt = "." + filterTokens[0];
+            var filterRoot = $@"{filterTokens[1]}\";
+
+            Dictionary<string, decimal> filePlusSize = new Dictionary<string, decimal>();
+
+            foreach (var f in allFiles)
+            {
+                var pathPlusSize = f.Split(';');
+                var currentFilePath = pathPlusSize[0];
+                decimal currentFileSize = decimal.Parse(pathPlusSize[1]);
+
+                if (currentFilePath.StartsWith(filterRoot) && currentFilePath.EndsWith(filterExt))
+                {
+                    var tokens = currentFilePath.Split('\\');;
+                    var fileName = tokens[tokens.Length - 1];
+
+                    filePlusSize[fileName] = currentFileSize;
+                }
+            }
+
+            var sortedOutputFiles = filePlusSize.OrderByDescending(fs => fs.Value).ThenBy(fs => fs.Key);
+
+            if (sortedOutputFiles.Count() > 0)
+            {
+                foreach (var file in sortedOutputFiles)
+                {
+                    Console.WriteLine(file.Key + " - " + file.Value + " KB");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
