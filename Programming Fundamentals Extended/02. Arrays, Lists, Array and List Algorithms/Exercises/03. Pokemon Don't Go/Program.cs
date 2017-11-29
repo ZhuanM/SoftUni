78d5c25bae6d178b80d32562929781b632be6241
Zhuan <jowan@abv.bg>
2017-11-29 21:00:59 +0300

    array exercises almost done

--- Programming Fundamentals Extended/02. Arrays, Lists, Array and List Algorithms/Exercises/03. Pokemon Don't Go/Program.cs
+++ Programming Fundamentals Extended/02. Arrays, Lists, Array and List Algorithms/Exercises/03. Pokemon Don't Go/Program.cs
@@ -10,7 +10,60 @@ namespace _03.Pokemon_Don_t_Go
     {
         static void Main(string[] args)
         {
+            List<int> elements = Console.ReadLine().Split().Select(int.Parse).ToList();
 
+            List<int> removedElements = new List<int>();
+
+            while (elements.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+
+                if (index < 0)
+                {
+                    index = 0;
+                    removedElements.Add(elements[index]);
+                    elements.RemoveAt(index);
+                    int lastElement = elements[elements.Count - 1];
+                    elements.Insert(0, lastElement);
+
+                    IncreaseAndDecrease(elements, removedElements);
+                }
+                else if (index > elements.Count - 1)
+                {
+                    index = elements.Count - 1;
+                    removedElements.Add(elements[index]);
+                    elements.RemoveAt(index);
+                    int firstElement = elements[0];
+                    elements.Insert(index, firstElement);
+
+
+                    IncreaseAndDecrease(elements, removedElements);
+                }
+                else
+                {
+                    removedElements.Add(elements[index]);
+                    elements.RemoveAt(index);
+
+                    IncreaseAndDecrease(elements, removedElements);
+                }   
+            }
+
+            Console.WriteLine(removedElements.Sum());
+        }
+
+        static void IncreaseAndDecrease(List<int> elements, List<int> removedElements)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] <= removedElements[removedElements.Count - 1])
+                {
+                    elements[i] += removedElements[removedElements.Count - 1];
+                }
+                else if (elements[i] > removedElements[removedElements.Count - 1])
+                {
+                    elements[i] -= removedElements[removedElements.Count - 1];
+                }
+            }
         }
     }
 }
---
+++ Programming Fundamentals Extended/03.2. Pokemon Don't Go/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._2.Pokemon_Don_t_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> elements = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            long sum = 0;
+            while (elements.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+
+                int element;
+                if (index < 0)
+                {
+                    element = elements[0];
+                    elements[0] = elements[elements.Count - 1];
+                }
+                else if (index >= elements.Count)
+                {
+                    element = elements[elements.Count - 1];
+                    elements[elements.Count - 1] = elements[0];
+                }
+                else
+                {
+                    element = elements[index];
+                    elements.RemoveAt(index);
+                }
+
+                sum += element;
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    if (elements[i] <= element)
+                    {
+                        elements[i] += element;
+                    }
+                    else
+                    {
+                        elements[i] -= element;
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
