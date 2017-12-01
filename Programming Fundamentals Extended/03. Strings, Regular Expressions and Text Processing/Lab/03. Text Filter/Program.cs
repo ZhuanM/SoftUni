99d9bce270cc5f9c83b0f7d19e454f0a18eaff70
Zhuan <jowan@abv.bg>
2017-12-01 21:03:18 +0300

    string and regex lab half-way done

---
+++ Programming Fundamentals Extended/03. Strings, Regular Expressions and Text Processing/Lab/03. Text Filter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordsToRemomv = Console.ReadLine();
+            string[] words = wordsToRemomv.Split(new[] {',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            foreach (var word in words)
+            {
+                string newWord = new string('*', word.Length);
+                text = text.Replace(word, newWord);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
