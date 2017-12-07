4774de4d44226bb9293551d29c1acb0f0d425d79
Zhuan <jowan@abv.bg>
2017-12-07 21:14:21 +0300

    dictionaries, linq, lambda expressions exercises supposedly done

--- Programming Fundamentals Extended/04. Dictionaries, LINQ and Lambda/Exercises/05. Pokemon Evolution/Program.cs
+++ Programming Fundamentals Extended/04. Dictionaries, LINQ and Lambda/Exercises/05. Pokemon Evolution/Program.cs
@@ -6,11 +6,93 @@ using System.Threading.Tasks;
 
 namespace _05.Pokemon_Evolution
 {
+    class Pokemon
+    {
+        internal class Evolution
+        {
+            public string Name { get; set; }
+            public int Index { get; set; }
+
+            public Evolution(string name, int index)
+            {
+                Name = name;
+                Index = index;
+            }
+        }
+        public string Name { get; set; }
+        public List<Evolution> Evolutions { get; set; }
+
+        public Pokemon(string name)
+        {
+            Name = name;
+            Evolutions = new List<Evolution>();
+        }
+
+        public void AddEvolution(string evolution, int index)
+        {
+            Evolutions.Add(new Evolution(evolution, index));
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var pokemonDict = new Dictionary<string, Pokemon>();
 
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "wubbalubbadubdub")
+                {
+                    foreach (Pokemon pokemon in pokemonDict.Values)
+                    {
+                        string printResult = $"# {pokemon.Name}";
+                        foreach (var evolution in pokemon.Evolutions
+                            .OrderByDescending(e => e.Index))
+                        {
+                            printResult += $"\n{evolution.Name} <-> {evolution.Index}";
+                        }
+
+                        Console.WriteLine(printResult);
+                    }
+
+                    break;
+                }
+
+                if (pokemonDict.ContainsKey(input))
+                {
+                    string pokemonName = input;
+                    string printResult = $"# {pokemonName}";
+
+                    foreach (var evolution in pokemonDict[pokemonName].Evolutions)
+                    {
+                        printResult += $"\n{evolution.Name} <-> {evolution.Index}";
+                    }
+
+                    Console.WriteLine(printResult);
+                }
+                else
+                {
+                    string[] inputTokens = input
+                        .Split(new[] { '-', ' ', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // If we actually wanted to get pokemon info
+                    // but that pokemon didn't exist in the dictionary
+                    // we shouldn't add a new pokemon but just continue.
+                    if (inputTokens.Length != 3)
+                        continue;
+
+                    string pokemonDictName = inputTokens[0];
+                    string evolutionType = inputTokens[1];
+                    int evolutionIndex = int.Parse(inputTokens[2]);
+
+                    if (!pokemonDict.ContainsKey(pokemonDictName))
+                        pokemonDict.Add(pokemonDictName, new Pokemon(pokemonDictName));
+
+                    pokemonDict[pokemonDictName].AddEvolution(evolutionType, evolutionIndex);
+                }
+            }
         }
     }
 }
--- Programming Fundamentals Extended/Test/Program.cs
+++ Programming Fundamentals Extended/Test/Program.cs
@@ -10,7 +10,63 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            //Pokemon Evolution exercise
+            var pokemonDict = new Dictionary<string, Dictionary<string, List<long>>>();
 
+            string input = Console.ReadLine();
+
+            while (input != "wubbalubbadubdub")
+            {
+                string[] tokens = input.Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens.Length > 1) //{pokemonName} -> {evolutionType} -> {evolutionIndex}
+                {
+                    string pokemonName = tokens[0];
+                    string evolutionType = tokens[1];
+                    long evolutionIndex = long.Parse(tokens[2]);
+
+                    if (!pokemonDict.ContainsKey(pokemonName))
+                    {
+                        pokemonDict.Add(pokemonName, new Dictionary<string, List<long>>());
+                    }
+                    if (!pokemonDict[pokemonName].ContainsKey(evolutionType))
+                    {
+                        pokemonDict[pokemonName].Add(evolutionType, new List<long>());
+                    }
+                    pokemonDict[pokemonName][evolutionType].Add(evolutionIndex);
+
+
+                }
+                else if (tokens.Length == 1) //Only pokemon name
+                {
+                    string pokemonName = tokens[0];
+
+                    if (pokemonDict.ContainsKey(pokemonName))
+                    {
+                        Console.WriteLine($"# {pokemonName}");
+                        foreach (var item in pokemonDict[pokemonName])
+                        {
+                            Console.WriteLine($"{item.Key} <-> {item.Value}");
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            //OUTPUT
+            foreach (var item in pokemonDict)
+            {
+                Console.WriteLine($"# {item.Key}");
+                if (item.Value.Values.Count > 1)
+                {
+
+                }
+                foreach (var inner in item.Value)
+                {
+                    Console.WriteLine($"{inner.Key} <-> {inner.Value}");
+                }
+            }
         }
     }
 }
