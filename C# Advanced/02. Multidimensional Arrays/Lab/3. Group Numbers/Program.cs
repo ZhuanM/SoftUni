561224eefbc6a191923c7576e5b352ae9436bac7
Zhuan <jowan@abv.bg>
2018-01-28 22:40:39 +0300

    multidimensional arrays lab done

--- C# Advanced/02. Multidimensional Arrays/Lab/3. Group Numbers/Program.cs
+++ C# Advanced/02. Multidimensional Arrays/Lab/3. Group Numbers/Program.cs
@@ -10,7 +10,43 @@ namespace _3.Group_Numbers
     {
         static void Main(string[] args)
         {
+            int[] numbers = Console.ReadLine()
+                .Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
+            int[] sizes = new int[3];
+
+            foreach (var number in numbers)
+            {
+                sizes[Math.Abs(number % 3)]++; // It's Math Abs because in case we are given negative int as input
+                // Gets the count of each group (0, 1, 2) which are the remainder of the number when divided by 3 so we can know the size of the jagged arrays
+            }
+
+            int[][] jaggedArray = new int[3][];
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                jaggedArray[i] = new int[sizes[i]]; // This loop is so we can initialize all arrays so we can use them later on
+            }
+
+            int[] index = new int[3];
+            foreach (var number in numbers)
+            {
+                int remainder = Math.Abs(number % 3); // It's Math Abs because in case we are given negative int as input
+                jaggedArray[remainder][index[remainder]] = number;
+                index[remainder]++; // It's so we can move to the next index depending on which row we are at (0, 1, 2)
+            }
+
+            // PRINTING
+            for (int rows = 0; rows < jaggedArray.Length; rows++)
+            {
+                for (int columns = 0; columns < jaggedArray[rows].Length; columns++)
+                {
+                    Console.Write(jaggedArray[rows][columns] + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
--- C# Advanced/02. Multidimensional Arrays/Lab/4. Pascal Triangle/Program.cs
+++ C# Advanced/02. Multidimensional Arrays/Lab/4. Pascal Triangle/Program.cs
@@ -10,7 +10,38 @@ namespace _4.Pascal_Triangle
     {
         static void Main(string[] args)
         {
+            int height = int.Parse(Console.ReadLine());
 
+            long[][] triangle = new long[height][]; // It's long because if we enter 60 as input it will overflow at the end
+
+            for (int currentHeight = 0; currentHeight < height; currentHeight++)
+            {
+                triangle[currentHeight] = new long[currentHeight + 1];
+
+                triangle[currentHeight][0] = 1; // The first row which is static
+                triangle[currentHeight][currentHeight] = 1; // The second row which is static
+
+                if (currentHeight >= 2) // From the third row and onwards
+                {
+                    for (int widthCounter = 1; widthCounter < currentHeight; widthCounter++) // We start from 1 and end on minus 1 because we already have them filled with 1's
+                    {
+                        // This makes sense if you look at the presentation gif for forming the pascal triangle
+                        triangle[currentHeight][widthCounter] =
+                            triangle[currentHeight - 1][widthCounter - 1] +
+                            triangle[currentHeight - 1][widthCounter];
+                    }
+                }
+            }
+
+            // PRINTING
+            for (int rows = 0; rows < triangle.Length; rows++)
+            {
+                for (int columns = 0; columns < triangle[rows].Length; columns++)
+                {
+                    Console.Write(triangle[rows][columns] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
