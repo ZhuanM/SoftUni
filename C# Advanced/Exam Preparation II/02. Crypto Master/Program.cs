ece093466a95f9dfb457f2341c0b6b5882b6262d
Zhuan <jowan@abv.bg>
2018-02-10 20:41:48 +0200

    exam prep II done

---
+++ C# Advanced/Exam Preparation II/02. Crypto Master/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Crypto_Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short[] array = Console.ReadLine()
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(short.Parse)
+                .ToArray();
+
+            int longestRun = 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                for (int step = 1; step < array.Length; step++)
+                {
+                    int currentIndex = i;
+
+                    int nextIndex = (currentIndex + step) % array.Length;
+                    int currentRun = 1;
+
+                    while (array[nextIndex] > array[currentIndex])
+                    {
+                        currentRun++;
+                        if (currentRun > longestRun)
+                        {
+                            longestRun = currentRun;
+                        }
+
+                        currentIndex = nextIndex;
+                        nextIndex = (currentIndex + step) % array.Length;
+                    }
+                }
+            }
+
+            Console.WriteLine(longestRun);
+        }
+    }
+}
