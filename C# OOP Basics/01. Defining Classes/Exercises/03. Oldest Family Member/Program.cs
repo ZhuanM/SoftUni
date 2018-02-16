c432a47e1fc2831ef5f9d5b649628f09858e1982
Zhuan <jowan@abv.bg>
2018-02-16 23:13:50 +0200

    defining classes exercises started

---
+++ C# OOP Basics/01. Defining Classes/Exercises/03. Oldest Family Member/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Family family = new Family();
+
+        int peopleCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < peopleCount; i++)
+        {
+            string[] inputTokens = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            string name = inputTokens[0];
+            int age = int.Parse(inputTokens[1]);
+
+            Person person = new Person(name, age);
+
+            family.AddMember(person);
+        }
+
+        Person oldestPerson = family.GetOldestMember();
+
+        Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+    }
+}
---
+++ C# OOP Basics/01. Defining Classes/Exercises/04. Opinion Poll/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>();
+
+        int peopleCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < peopleCount; i++)
+        {
+            string[] inputeTokens = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            string name = inputeTokens[0];
+            int age = int.Parse(inputeTokens[1]);
+
+            Person person = new Person(name, age);
+
+            people.Add(person);
+        }
+
+        foreach (var person in people.Where(p => p.Age > 30).OrderBy(p => p.Name))
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+    }
+}
