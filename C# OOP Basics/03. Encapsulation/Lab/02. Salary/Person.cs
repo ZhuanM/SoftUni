24efef0197605d0e7d735d21825d11cf9e009a72
Zhuan <jowan@abv.bg>
2018-02-21 11:11:13 +0200

    encapsulation lab almost done

---
+++ C# OOP Basics/03. Encapsulation/Lab/02. Salary/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private decimal salary;
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+        set { this.firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return this.lastName; }
+        set { this.lastName = value; }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        set { this.age = value; }
+    }
+
+    public decimal Salary
+    {
+        get { return this.salary; }
+        set { this.salary = value; }
+    }
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(string firstName, string lastName, int age, decimal salary)
+        : this(firstName, lastName, age)
+    {
+        this.salary = salary;
+    }
+
+    public void IncreaseSalary(decimal percentage)
+    {
+        if (this.Age > 30)
+        {
+            salary = salary + salary * (percentage / 100);
+        }
+        else
+        {
+            salary = salary + salary * (percentage / 200); // half the salary
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+    }
+}
