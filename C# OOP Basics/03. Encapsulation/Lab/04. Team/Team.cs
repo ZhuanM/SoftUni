710bc73f955e9a8bbdd4d461063b39928b32b298
Zhuan <jowan@abv.bg>
2018-02-21 21:56:29 +0200

    encapsulation lab done

---
+++ C# OOP Basics/03. Encapsulation/Lab/04. Team/Team.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Team
+{
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+    private string name;
+
+    public IReadOnlyCollection<Person> FirstTeam
+    {
+        get { return this.firstTeam; }
+    }
+
+    public IReadOnlyCollection<Person> ReserveTeam
+    {
+        get { return reserveTeam; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public Team(string name)
+    {
+        this.firstTeam = new List<Person>();
+        this.reserveTeam = new List<Person>();
+        this.name = name;
+    }
+
+    public void AddPlayer(Person person)
+    {
+        if (person.Age < 40)
+        {
+            this.firstTeam.Add(person);
+        }
+        else
+        {
+            this.reserveTeam.Add(person);
+        }
+    }
+}
