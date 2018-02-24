4275bb89ec8103bcedbfa23e04923bdfa2cdc2bb
Zhuan <jowan@abv.bg>
2018-02-24 18:31:52 +0200

    inheritance lab done

---
+++ C# OOP Basics/04. Inheritance/Lab/04. Random List/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class RandomList : List<string>
+{
+    Random random = new Random();
+
+    public string RandomString()
+    {
+        string result = null;
+
+        if (this.Count > 0)
+        {
+            int randomIndex = random.Next(0, this.Count - 1);
+
+            result = this[randomIndex]; // So we can return the string that we removed
+
+            this.RemoveAt(randomIndex);
+        }
+
+        return result;
+    }
+}
---
+++ C# OOP Basics/04. Inheritance/Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class StackOfStrings
+{
+    List<string> data = new List<string>();
+
+    public bool IsEmpty()
+    {
+        return data.Count == 0; // Returns True or False according to whether data.Count is 0 or not
+    }
+
+    public void Push(string item)
+    {
+        data.Add(item);
+    }
+
+    public string Peek()
+    {
+        string result = string.Empty;
+        if (!IsEmpty()) // If list NOT empty
+        {
+            int lastIndex = data.Count - 1;
+            result = data[lastIndex];
+        }
+
+        return result;
+    }
+
+    public string Pop()
+    {
+        string result = string.Empty;
+        if (!IsEmpty()) // If list NOT empty
+        {
+            int lastIndex = data.Count - 1;
+            result = data[lastIndex];
+
+            data.RemoveAt(lastIndex);
+        }
+
+        return result;
+    }
+}
