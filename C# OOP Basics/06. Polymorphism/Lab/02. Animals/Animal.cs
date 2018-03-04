bad7f38438cddaedbcd31fac9233f994248f72a5
Zhuan <jowan@abv.bg>
2018-03-04 22:46:46 +0200

    polymorphism lab almost done

---
+++ C# OOP Basics/06. Polymorphism/Lab/02. Animals/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public abstract class Animal
+{
+    public Animal(string name, string favouriteFood)
+    {
+        this.Name = name;
+        this.FavouriteFood = favouriteFood;
+    }
+
+    public string Name { get; set; }
+
+    public string FavouriteFood { get; set; }
+
+    public virtual string ExplainSelf()
+    {
+        var result = $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+
+        return result;
+    }
+}
