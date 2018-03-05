deb4fe4f2342bd045fdeea4b51807f31a493a771
Zhuan <jowan@abv.bg>
2018-03-05 15:14:00 +0200

    polymorphism lab done

---
+++ C# OOP Basics/06. Polymorphism/Lab/03. Shapes/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.Radius = radius;
+    }
+
+    public double Radius
+    {
+        get => this.radius;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException
+                    (nameof(this.Radius), "Circle cannot have a zero or negative radius!");
+            }
+
+            this.radius = value;
+        }
+    }
+
+    public override double CalculateArea()
+    {
+        double area = Math.PI * this.Radius * this.Radius;
+
+        return area;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        double perimeter = 2 * Math.PI * this.Radius;
+
+        return perimeter;
+    }
+
+    public override string Draw()
+    {
+        return base.Draw() + "Circle";
+    }
+}
---
+++ C# OOP Basics/06. Polymorphism/Lab/03. Shapes/Rectangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Rectangle : Shape
+{
+    private double sideA;
+    private double sideB;
+
+    public Rectangle(double sideA, double sideB)
+    {
+        this.SideA = sideA;
+        this.SideB = sideB;
+    }
+
+    public double SideA
+    {
+        get => this.sideA;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException
+                    (nameof(this.SideA), "Side A must be a non-zero and positive");
+            }
+
+            this.sideA = value;
+        }
+    }
+
+    public double SideB
+    {
+        get => this.sideB;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException
+                    (nameof(this.SideB), "Side B must be a non-zero and positive");
+            }
+
+            this.sideB = value;
+        }
+    }
+
+    public override double CalculateArea()
+    {
+        double area = this.SideA * this.SideB;
+
+        return area;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        double perimeter = (this.SideA + this.SideB) * 2;
+
+        return perimeter;
+    }
+
+    public override string Draw()
+    {
+        return base.Draw() + "Rectangle";
+    }
+}
