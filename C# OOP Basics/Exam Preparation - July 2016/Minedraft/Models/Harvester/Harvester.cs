b9c5250e604a6c584e605502b533fe322b196dd8
Zhuan <jowan@abv.bg>
2018-03-15 09:48:04 +0200

    exam prep first task done

---
+++ C# OOP Basics/Exam Preparation - July 2016/Minedraft/Models/Harvester/Harvester.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public abstract class Harvester : Model
+{
+    private const double MAX_ENERGY_REQUIREMENT = 20_000;
+
+    private double oreOutput;
+    private double energyRequirement;
+
+    public double OreOutput
+    {
+        get { return oreOutput; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException($"Harvester is not registered, because of it's OreOutput");
+            }
+            oreOutput = value;
+        }
+    }
+
+    public double EnergyRequirement
+    {
+        get { return energyRequirement; }
+        private set
+        {
+            if (value < 0 || value > MAX_ENERGY_REQUIREMENT)
+            {
+                throw new ArgumentException("Harvester is not registered, because of it's EnergyRequirement");
+            }
+            energyRequirement = value;
+        }
+    }
+
+    protected Harvester(string id, double oreOutput, double energyRequirement)
+        : base(id)
+    {
+        this.OreOutput = oreOutput;
+        this.EnergyRequirement = energyRequirement;
+    }
+}
---
+++ C# OOP Basics/Exam Preparation - July 2016/Minedraft/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public abstract class Model
+{
+    public string Id { get; private set; }
+
+    protected Model(string id) // It's protected because the derived classes should be able to chain the constructor (and it's not public because it's not going to be initialized)
+    {
+        this.Id = id;
+    }
+}
