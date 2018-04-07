592ced17052f2081deaa532cfff7115a5b9c1a95
Zhuan <jowan@abv.bg>
2018-04-07 22:58:25 +0200

    unit testing lab done

---
+++ C# OOP Advanced/05. Unit Testing/Lab/SkeletonTests/AxeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SkeletonTests
+{
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            Axe axe = new Axe(5, 10);
+            Dummy dummy = new Dummy(20, 20);
+
+            axe.Attack(dummy);
+
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void BrokenAxeCannotAttack()
+        {
+            Axe axe = new Axe(5, 0);
+            Dummy dummy = new Dummy(20, 20);
+
+            Assert.That(() => axe.Attack(dummy),
+                Throws.InvalidOperationException.
+                With.Message.EqualTo(("Axe is broken.")));
+        }
+    }
+}
---
+++ C# OOP Advanced/05. Unit Testing/Lab/SkeletonTests/DummyTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SkeletonTests
+{
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLosesHealthAfterAttack()
+        {
+            const int initialHealth = 10;
+            const int attackToTake = 5;
+            const int expectedHealth = 5;
+
+            Dummy dummy = new Dummy(initialHealth, 20);
+
+            dummy.TakeAttack(attackToTake);
+
+            Assert.That(dummy.Health, Is.EqualTo(expectedHealth));
+            //Assert.AreEqual(expectedHealth, dummy.Health); THIS GIVES THE SAME RESULTS AS THE LINE ABOVE
+        }
+
+        [Test]
+        public void AttackDeadDummyShouldThrow()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.That(() => dummy.TakeAttack(5),
+                Throws.InvalidOperationException
+                .With.Message.EqualTo("Dummy is dead."));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveExperience()
+        {
+            Hero hero = new Hero("Zhuan");
+            Dummy dummy = new Dummy(10, 50);
+
+            hero.Attack(dummy);
+
+            Assert.That(hero.Experience, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void AliveDummyCantGiveExperience()
+        {
+            Hero hero = new Hero("Zhuan");
+            Dummy dummy = new Dummy(20, 50);
+
+            hero.Attack(dummy);
+
+            Assert.That(hero.Experience, Is.EqualTo(0));
+        }
+    }
+}
